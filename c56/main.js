a08b9e07eb01cba8dc8f31efa4545aa284976fdf
Taanish08 <123229789+Taanish08@users.noreply.github.com>
2023-01-21 09:24:58 +0300

    Add files via upload

---
+++ c56/main.js
@@ -0,0 +1,10 @@
+function ct() {
+    document.getElementById("heading1").style.color="red";
+    document.getElementById("p1").style.color="blue";
+    document.getElementById("bd").style.backgroundColor="orange";
+}
+function yes() {
+    document.getElementById("heading1").style.color="orange";
+    document.getElementById("p1").style.color="red";
+    document.getElementById("bd").style.backgroundColor="lightblue";   
+}
